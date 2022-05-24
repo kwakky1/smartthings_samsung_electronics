b90d282df02229ffbcd6a1f497ebc91c53742652
Andy Kwak <kwakky1@gmail.com>
2022-05-24 16:54:29 +0900

    setting fix

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'SamsungAirPurifier';
+export const PLATFORM_NAME = 'SmartthingsSamsungElectronics';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-samsung-air-purifier';
+export const PLUGIN_NAME = 'homebridge-smartthings-samsung-electronics';
